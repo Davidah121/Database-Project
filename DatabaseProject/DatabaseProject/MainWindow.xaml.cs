aa31a481dac361046b076d746f0297c5b9d82adb
hadley31 <nick.hadley31@gmail.com>
2019-04-14 14:31:45 -0400

    getting things ready to start production

---
+++ DatabaseProject/DatabaseProject/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DatabaseProject
+{
+    public partial class MainWindow : Window
+    {
+        List<Grid> menus;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            // Initialize list of all menus
+            menus = new List<Grid>()
+            {
+                menu_main,
+                menu_employees,
+                menu_animals,
+                menu_donations,
+                menu_deliveries,
+                menu_trans,
+                menu_expmode
+            };
+
+
+            // Open the main menu
+            OpenMenu(menu_main);
+        }
+
+
+        private void OpenMenu(Grid menu)
+        {
+            if (menu == null)
+            {
+                return;
+            }
+
+
+            // Loop through all the menus. If the menu is the one specified, set it to visible. Otherwise, hidden
+            // This is a self correcting system
+            foreach (Grid m in menus)
+            {
+                m.Visibility = m == menu ? Visibility.Visible : Visibility.Hidden;
+            }
+        }
+    }
+}
---
+++ DatabaseProject/DatabaseProject/Menus/ExpertModeMenu.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseProject
+{
+    partial class MainWindow : Window
+    {
+        const string connectionString = "Data Source=NICK-PC;Initial Catalog=MyGuitarShop;Integrated Security=True";
+
+        #region Events
+
+
+        private void Btn_expmode_submit_Click(object sender, RoutedEventArgs e)
+        {
+            string cmd = textbox_expmode_sql.Text;
+
+            if (String.IsNullOrWhiteSpace(cmd))
+            {
+                return;
+            }
+
+            Submit(cmd);
+        }
+
+
+        #endregion
+
+
+        private void Submit(string query)
+        {
+            if (query.StartsWith("select", StringComparison.OrdinalIgnoreCase))
+            {
+                datagrid_expmode.ItemsSource = Query(query)?.DefaultView;
+            }
+            else
+            {
+                NonQuery(query);
+            }
+        }
+
+
+        private DataTable Query(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    // Open the connection to the database
+                    connection.Open();
+
+
+                    // Create an adapter object. This will help us retrieve the rows from the database
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+
+                    // Create an new table
+                    DataTable table = new DataTable();
+
+
+                    // Populate the table with the rows from the database
+                    adapter.Fill(table);
+
+                    return table;
+                }
+                catch
+                {
+                    MessageBox.Show("Something went wrong!");
+                    return null;
+                }
+            }
+        }
+
+
+        private void NonQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Non-query executed successfully!");
+                }
+                catch
+                {
+                    MessageBox.Show("Something went wrong!");
+                }
+            }
+        }
+    }
+}
