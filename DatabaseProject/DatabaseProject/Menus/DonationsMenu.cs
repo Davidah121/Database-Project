8d6714306a1af91d8b2362fc019411993d24e3c3
hadley31 <nick.hadley31@gmail.com>
2019-04-15 14:55:38 -0400

    changes

--- DatabaseProject/DatabaseProject/Menus/DonationsMenu.cs
+++ DatabaseProject/DatabaseProject/Menus/DonationsMenu.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace DatabaseProject
 {
@@ -20,7 +21,34 @@ namespace DatabaseProject
         #region Events
 
 
+        private void Btn_new_donation_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("New donation");
 
+            DataTable table = Query($"select * from Addresses where CustomerID = {textbox_donator_id.Text}");
+
+            if (table == null)
+            {
+                return;
+            }
+
+            MessageBox.Show(table.Rows[0]["Line1"].ToString());
+        }
+
+        private void Btn_view_donation_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("View donation");
+        }
+
+        private void Btn_update_donation_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Update donation");
+        }
+
+        private void Btn_delete_donation_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Delete donation");
+        }
 
 
         #endregion
